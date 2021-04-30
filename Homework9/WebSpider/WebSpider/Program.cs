67416ef5938e030588aeffc61dc6254329e1aaf4
赵皓宇 <55451421+zhaohaoyu376@users.noreply.github.com>
2021-04-30 11:40:16 +0800

    Add files via upload

---
+++ Homework9/WebSpider/WebSpider/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebSpider
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
